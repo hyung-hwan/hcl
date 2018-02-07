accc9a05096d7cfc6d7c5dd70eb36e4ff4b099d0
hyunghwan.chung <hyunghwan.chung@a52697d0-ac12-4f7d-b698-8af0109f4dd6>
2018-02-07 14:13:13 +0000

    changed copyright years
    git-svn-id: https://code.abiyo.net/svn/hcl/trunk@125 a52697d0-ac12-4f7d-b698-8af0109f4dd6

--- hcl/lib/bigint.c
+++ hcl/lib/bigint.c
@@ -2,7 +2,7 @@
 /*
  * $Id$
  *
-    Copyright (c) 2014-2016 Chung, Hyung-Hwan. All rights reserved.
+    Copyright (c) 2016-2018 Chung, Hyung-Hwan. All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
     modification, are permitted provided that the following conditions
@@ -3711,7 +3711,7 @@ oops_einval:
 /*
  * $Id$
  *
-    Copyright (c) 2014-2017 Chung, Hyung-Hwan. All rights reserved.
+    Copyright (c) 2016-2018 Chung, Hyung-Hwan. All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
     modification, are permitted provided that the following conditions
